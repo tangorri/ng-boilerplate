7bb78dd28e4bed7d423da7a04c67177b459f7a90
Antoine Malpel <contact@tangorri.net>
2013-11-21 20:03:45 +0100

    Karma target to Chrome instead of Firefox

--- karma/karma-unit.tpl.js
+++ karma/karma-unit.tpl.js
@@ -55,7 +55,8 @@ module.exports = function ( karma ) {
      * the aesthetic advantage of not launching a browser every time you save.
      */
     browsers: [
-      'Firefox'
+      // 'Firefox'
+      'Chrome'
     ]
   });
 };
